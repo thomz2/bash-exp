c7edf98d3f992876edd74cd2b64a58117f110d7b
Thomaz <82160387+thomz2@users.noreply.github.com>
2022-09-01 10:31:27 -0300

    Update comp_replit.sh

--- comp_replit.sh
+++ comp_replit.sh
@@ -3,28 +3,41 @@
 shopt -s extdebug
 
 argv () {
-  if test ! -f ./${BASH_ARGV[$#-1]}
-  then   
-    # compilar
-    local compilar="ocamlopt -o"
-    for (( i=$#-1; i > -1; i-- )) 
-    do
-      compilar+=" ${BASH_ARGV[i]}"
-    done
-    $compilar
+  
+  # adicionar OR testando se o arquivo atual é diferente do oculto
 
-    # remover arquivos desnecessarios
-    if [[ "${BASH_ARGV[$#-1]}" == *"/"* ]]
+  for (( i=$#-2; i > -1; i-- ))
+  do 
+    if ! cmp -s ${BASH_ARGV[i]} ".${BASH_ARGV[i]}" || test ! -f ./${BASH_ARGV[$#-1]}
     then 
-      rm -vf ${BASH_ARGV[$#-1]%/*}/*.o ${BASH_ARGV[$#-1]%/*}/*.cm* 
-    else
-      rm -vf *\.o  *\.cm*
-    fi 
-  fi 
+      # se achou algum diferente ou nao existe arquivo compilado, compilo
+      # compilar
+      local compilar="ocamlopt -o"
+      for (( i=$#-1; i > -1; i-- )) 
+      do
+        compilar+=" ${BASH_ARGV[i]}"
+      done
+      $compilar
+  
+      # remover arquivos desnecessarios
+      if [[ "${BASH_ARGV[$#-1]}" == *"/"* ]]
+      then 
+        rm -vf ${BASH_ARGV[$#-1]%/*}/*.o ${BASH_ARGV[$#-1]%/*}/*.cm* 
+      else
+        rm -vf *\.o  *\.cm*
+      fi 
+      break
+    fi
+  done
 
   # executar
   "./${BASH_ARGV[$#-1]}"
 
+  # criar arquivo(s) oculto(s) contendo codigo 
+  for (( i=$#-2; i > -1; i-- )) # pegando apenas arquivos .ml
+  do
+    cp ${BASH_ARGV[i]} ".${BASH_ARGV[i]}" 
+  done
 }
 
 argv "$@"
